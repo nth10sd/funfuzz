09ec7ed36d7bdd8d3f01f5b8478129dde5cfa5f0
Gary Kwong <nth10sd@gmail.com>
2018-06-15 11:53:29 -0700

    Skip input mode when rev is not default, for patch fuzzing.

--- src/funfuzz/util/hg_helpers.py
+++ src/funfuzz/util/hg_helpers.py
@@ -133,9 +133,9 @@ def get_repo_hash_and_id(repo_dir, repo_rev="parents() and default"):
         ).stdout.decode("utf-8", errors="replace")
     is_on_default = bool(hg_id_full)
     if not is_on_default:
-        # pylint: disable=input-builtin
-        update_default = input("Not on default tip! "
-                               "Would you like to (a)bort, update to (d)efault, or (u)se this rev: ")
+        # update_default = input("Not on default tip! "
+        #                        "Would you like to (a)bort, update to (d)efault, or (u)se this rev: ")
+        update_default = "u"
         update_default = update_default.strip()
         if update_default == "a":
             print("Aborting...")
