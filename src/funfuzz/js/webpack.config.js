bad97c47651152f805a1f9dd5e0308d7822939f1
Gary Kwong <nth10sd@gmail.com>
2019-07-31 15:24:37 -0700

    Add a webpack.config.js file using tail.js as an entry point.

---
+++ src/funfuzz/js/webpack.config.js
@@ -0,0 +1,16 @@
+/* global __dirname, module, require */
+
+var path = require("path"); /* eslint-disable-line import/no-commonjs,import/no-nodejs-modules,import/unambiguous */
+
+module.exports = { /* eslint-disable-line import/no-commonjs */
+  mode: "development",
+  entry: [
+    "./jsfunfuzz/tail.js"
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "jsfunfuzz.js"
+    // library: ["start"],
+    // libraryTarget: "var"
+  }
+}; /* eslint-disable-line import/no-commonjs */
